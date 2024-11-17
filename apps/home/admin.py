b8eae7f1be45cf1e31117bf2d73c3b82d5ab1571
Darcy Liu <darcy.ldx@gmail.com>
2024-11-17 18:28:08 -0500

    hello

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from home.models import Topic, Webpage, AccessRecord
+from apps.home.models import Topic, Webpage, AccessRecord
 # Register your models here.
 admin.site.register(Topic)
 admin.site.register(Webpage)
